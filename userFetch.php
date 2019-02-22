ec5ab5a55b2a70bc8cff3057ae4ba9bf0cfe892d
Akarsh Shrivastava <ak.shri48@gmail.com>
2019-02-22 17:25:30 +0530

    Formatted user Fetched details

--- userFetch.php
+++ userFetch.php
@@ -3,10 +3,10 @@ include_once('config.php');
 $result= mysqli_query($conn , "SELECT * FROM user");
 while ($row = mysqli_fetch_assoc($result)){
 	if($row['user_status'] == 1 ){
-		echo "<font color='#009900'>".$row['user_name']." (Online)"."</font><br>";
+		echo "<font color='#009900'>&nbsp;".$row['user_name']." (Online)"."</font><br>";
 		}
 		else {
-				echo "<font color='#FF0000'>".$row['user_name']." (Offline)"."</font><br>";
+				echo "<font color='#FF0000'>&nbsp;".$row['user_name']." (Offline)"."</font><br>";
 			}
 	}
 
