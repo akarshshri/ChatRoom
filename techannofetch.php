ca9a3bff16d2c11a4cf8404ddd50c6de81cfb100
Akarsh Shrivastava <ak.shri48@gmail.com>
2019-03-06 02:22:06 +0530

    Made technology screen more legible

--- techannofetch.php
+++ techannofetch.php
@@ -1,10 +1,10 @@
 <?php
 include_once('config.php');
 $result= mysqli_query($conn , "SELECT * FROM anno_tech");
-echo '<center><h3 >Announcement</h3></center>';
+echo '<center><h3 >Announcement</h3></center><hr>';
 while ($row = mysqli_fetch_assoc($result)){
 	//echo '<font size="4.5px"><b>'.$row['name']."</b></font> : ";
-	echo $row['anno']."<br>";
+	echo "<li>".$row['anno']."<br>";
 	
 	}
 ?>
--- userFetch.php
+++ userFetch.php
@@ -1,6 +1,7 @@
 <?php
 include_once('config.php');
 $result= mysqli_query($conn , "SELECT * FROM user");
+echo "<h3><center>Users</center></h3><hr>";
 while ($row = mysqli_fetch_assoc($result)){
 	if($row['user_status'] == 1 ){
 		echo "<font color='#009900'>&nbsp;".$row['user_name']." (Online)"."</font><br>";
