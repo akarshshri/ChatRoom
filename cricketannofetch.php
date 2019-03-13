dc1d9229f2274a274fde255774366425bc0eba3e
Akarsh <ak.shri48@gmail.com>
2019-03-13 10:40:00 +0530

    Announcement Feature added to all catagories

---
+++ cricketannofetch.php
@@ -0,0 +1,10 @@
+<?php
+include_once('config.php');
+$result= mysqli_query($conn , "SELECT * FROM anno_cricket");
+echo '<center><h3 >Announcement</h3></center><hr>';
+while ($row = mysqli_fetch_assoc($result)){
+	//echo '<font size="4.5px"><b>'.$row['name']."</b></font> : ";
+	echo "<li>".$row['anno']."<br>";
+	
+	}
+?>
