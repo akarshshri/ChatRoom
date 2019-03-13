41a560896aa33a124d5359dddb8d2cdbdf72645f
Akarsh Shrivastava <ak.shri48@gmail.com>
2019-03-13 08:18:39 +0530

    New Database

--- chatroomnew.sql
+++ chatroomnew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2019 at 06:10 AM
+-- Generation Time: Mar 13, 2019 at 03:47 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -25,6 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `anno_tech`
+--
+
+CREATE TABLE `anno_tech` (
+  `id` int(4) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `anno` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `anno_tech`
+--
+
+INSERT INTO `anno_tech` (`id`, `name`, `anno`) VALUES
+(10, 'Akarsh Shrivastava', 'sadxasdasd'),
+(11, 'Akarsh Shrivastava', 'sad'),
+(12, 'Akarsh Shrivastava', 'testing\n'),
+(13, 'Akarsh Shrivastava', 'rere'),
+(14, 'Akarsh Shrivastava', 'asd'),
+(15, 'Akarsh Shrivastava', 'this is a long paragraph to test');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `chat`
 --
 
@@ -40,7 +64,12 @@ CREATE TABLE `chat` (
 --
 
 INSERT INTO `chat` (`chat_id`, `chat_person_name`, `chat_value`, `chat_time`) VALUES
-(43, 'Akarsh Shrivastava', 'This chat is for Technology section.\n', '10:39:10');
+(43, 'Akarsh Shrivastava', 'This chat is for Technology section.\n', '10:39:10'),
+(44, 'Akarsh Shrivastava', 'testing', '17:15:17'),
+(45, 'Akarsh Shrivastava', 'https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png\n', '10:22:00'),
+(46, 'Akarsh Shrivastava', 'hello\n', '23:21:26'),
+(47, 'Akarsh Shrivastava', 'de', '01:37:02'),
+(48, 'Akarsh Shrivastava', 'This is a long sentence example This is a long sentence example This is a long sentence example ', '02:12:28');
 
 -- --------------------------------------------------------
 
@@ -120,7 +149,11 @@ CREATE TABLE `gaming` (
 --
 
 INSERT INTO `gaming` (`chat_id`, `chat_person_name`, `chat_value`, `chat_time`) VALUES
-(2, 'Akarsh Shrivastava', 'wertu.', '17:55:29');
+(2, 'Akarsh Shrivastava', 'wertu.', '17:55:29'),
+(3, 'Akarsh Shrivastava', 'testing', '10:11:53'),
+(4, 'Parth Jaiswal', 'sample text\n', '10:12:34'),
+(5, 'Akarsh Shrivastava', 'jakhskdh', '10:12:57'),
+(6, 'Parth Jaiswal', 'jhgsgd', '10:13:06');
 
 -- --------------------------------------------------------
 
@@ -143,13 +176,21 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `user_name`, `user_email`, `user_password`, `user_country`, `user_status`) VALUES
 (1, 'Akarsh Shrivastava', 'test@gmail.com', '123', '1', '1'),
-(3, 'Akarsh Shrivastava', 'test2@gmail.com', '123', '1', '0'),
-(4, 'Akarsh Shrivastava', 'test3@gmail.com', '123', '1', '0'),
-(5, 'Akarsh Shrivastava', 'test4@gmail.com', '123', '1', '0');
+(3, 'Shravan Kumar', 'test2@gmail.com', '123', '1', '1'),
+(4, 'Parth Jaiswal', 'test3@gmail.com', '123', '1', '0'),
+(5, 'James Mathew', 'test4@gmail.com', '123', '1', '0'),
+(9, 'qwe', 'qwe@asd.com', 'qwew', '1', '0'),
+(17, 'asd', 'asd@sad.com', 'asd', '1', '0');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `anno_tech`
+--
+ALTER TABLE `anno_tech`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `chat`
@@ -193,10 +234,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `anno_tech`
+--
+ALTER TABLE `anno_tech`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `country`
@@ -220,13 +267,13 @@ ALTER TABLE `football`
 -- AUTO_INCREMENT for table `gaming`
 --
 ALTER TABLE `gaming`
-  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
