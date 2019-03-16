929c184752f20f5c13e124b592127e408e642225
Akarsh Shrivastava <ak.shri48@gmail.com>
2019-03-16 12:38:04 +0530

    changes

--- chatFetch.php
+++ chatFetch.php
@@ -1,7 +1,9 @@
 <?php
 include_once('config.php');
 $result= mysqli_query($conn , "SELECT * FROM chat");
+
 while ($row = mysqli_fetch_assoc($result)){
+
 	echo '<font size="4.5px"><b>'.$row['chat_person_name']."</b></font> : ";
 	echo $row['chat_value']."<br>";
 	
